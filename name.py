36b089c3e6c6748128c1039af83dc10a907c3e13
sherin-rajan <sherin2499@gmail.com>
2026-03-30 11:05:27 +0530

    Changes made in name.py file

--- name.py
+++ name.py
@@ -3,7 +3,6 @@ age=27
 place="Pathanamthitta"
 print("My name is sherin and I am 27 years old")
 print("My name is",name,"and I am ",age,"years old")
-# You can add any line to display and can go to previoue one by vcs
 print(f"My name is {name} and I am {age} years old")
-# New changes made by backend branch.
+print(f"My name is {name} and I am {age} years old. I am from {place}")
 
