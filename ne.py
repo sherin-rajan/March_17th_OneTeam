6189775585e5098e86794115b18bfa5235b386e8
sherin-rajan <sherin2499@gmail.com>
2026-03-30 15:59:01 +0530

    made changes to ne.py

--- ne.py
+++ ne.py
@@ -2,16 +2,20 @@ print('Welcome to OneTeam')
 name=input('Enter your name : ')
 age=int(input('Enter your age : '))
 place=input('Enter your place : ')
-courses=['Python','AWS','Devops','Software Testing']
+courses=['Python','AWS3','Devops','Software Testing']
 selected_course=input('Course you need : ')
 if selected_course in courses:
     print('Your course is availabe')
 else:
-    print('Selected course is not available')
-if selected_course in courses:
-    print('Course fees is 5000rs.')
-    print(input('Are you ready to join? '))
-else:
-    print('Thank for visiting')
+    print('The course you selected is not available')
+decision=input('Are you ready to join?')
+if decision=='yes':
+    print('Thank you for selection OneTeam. Our team will call you. Please provide your phone number.')
+phone_number=input('Please enter your number : ')
+print('Thank you')
+    
+
+
+    
 
 
