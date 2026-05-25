58b3921ee168dd95d946b47c65166f032c996d3f
sherin-rajan <sherin2499@gmail.com>
2026-05-25 15:57:19 +0530

    js basis

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -1,3 +1,21 @@
 let name="Ebin"
 let age=27
 console.log(`Hello ${name}, You are ${age} years old`)
+
+let a=10
+let b=20
+
+let choice=3
+
+if(choice==1){
+    console.log(a+b)
+}
+else if(choice==2){
+    console.log(a*b)
+}
+else if(choice==3){
+    console.log(b-a)
+}
+else{
+    console.log("Invalid Input")
+}
