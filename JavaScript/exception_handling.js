43f4bb5eb9922bddadef74bf8ac329daa2044a0c
sherin-rajan <sherin2499@gmail.com>
2026-06-02 11:13:09 +0530

    exception handling

--- JavaScript/exception_handling.js
+++ JavaScript/exception_handling.js
@@ -4,3 +4,23 @@ console.log(a+b)
 
 let c=0
 console.log(a/c)
+
+try {
+    let a = 10;
+    let b = 0;
+
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+
+    let result = a / b;
+    console.log(result);
+} catch (error) {
+    console.log(error.message);
+}
+
+try {
+    console.log(userName);
+} catch (error) {
+    console.log(error.message);
+}
