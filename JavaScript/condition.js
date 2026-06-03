0350d1b467e18a32a2a32a41c3709ab9e24e66f0
sherin-rajan <sherin2499@gmail.com>
2026-06-03 18:25:12 +0530

    portfolio

---
+++ JavaScript/condition.js
@@ -0,0 +1,15 @@
+let day=3;
+switch(day){
+    case 1:console.log("Monday")
+           break
+    case 2:console.log("Monday")
+           break
+    case 3:console.log("Monday")
+           break
+    case 4:console.log("Monday")
+           break
+    case 5:console.log("Monday")
+           break
+    default:console.log("Wrong value")
+           break      
+}
