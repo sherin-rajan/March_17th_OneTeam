715a11d6570d334e6676f355cc749a3193fe2740
sherin-rajan <sherin2499@gmail.com>
2026-06-24 15:24:58 +0530

    pymsql features

--- MySQL/mysql_prgm.py
+++ MySQL/mysql_prgm.py
@@ -1,6 +1,11 @@
 import pymysql
 
-connection=pymysql.connect(user="root",password="Caleb@2499",host="localhost",database="MyPortalDB")
+connection=pymysql.connect(user="root",
+                           password="Caleb@2499",
+                           host="localhost",
+                           database="MyPortalDB",
+                           cursorclass=pymysql.cursors.DictCursor  # Sets return type to dictionary
+                           )
 track=connection.cursor() #cursor is to execute sql queries and transactions
 
 def addUser():
@@ -10,11 +15,38 @@ def addUser():
     track.execute(f"INSERT INTO users (full_name,email,DOB) VALUES('{name}','{email}','{date}')")
     connection.commit() #to save on the DB
 
+def viewUsers():
+    track.execute("SELECT * FROM users")
+    data=track.fetchall()
+    for user in data:
+        print(f"{user['id']}  {user['full_name']}  {user['email']}  {user['DOB']}")
+    connection.commit()
+
+def deleteUser():
+    id=int(input("Enter id : "))
+    track.execute(f"DELETE FROM users WHERE id={id}")
+    print("Person with id",id,'delected')
+    connection.commit()
+
+def viewById():
+    id=int(input("Enter id : "))
+    track.execute(f"SELECT * FROM users WHERE id={id}")
+    person=track.fetchone()
+    print(person)
+    connection.commit()
+    
+
 while True:
-    print("1.Add users\n5.Exit")
+    print("1.Add users\n2.View Users\n3.Delete User\n4.View By ID\n5.Exit")
     choice=int(input("Enter your choice : "))
     if choice==1:
         addUser()
+    elif choice==2:
+        viewUsers()
+    elif choice==3:
+        deleteUser()
+    elif choice==4:
+        viewById()
     elif choice==5:
         print("Exiting...")
         break
