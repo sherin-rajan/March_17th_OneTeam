3fa3884c7d1cfc76a28f4ac78ceada5b5f8c0ba2
sherin-rajan <sherin2499@gmail.com>
2026-08-04 17:11:58 +0530

    created base.html

--- Django/netflix/movies/urls.py
+++ Django/netflix/movies/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from movies import views
 
 urlpatterns=[
-    path('',views.home),
-    path('add-category/',views.addCategory),
-    path('all-movies/',views.allMovies),
-    path('movie-detail/<int:movie_id>',views.movieDetails),
-    path('delete-movie/<int:movie_id>',views.deleteMovie),
-    path('add-movie/',views.addMovies),
-    path('update-movie/<int:movie_id>',views.updateMovie),
-    path('add-cast/',views.addCast)
+    path('',views.home,name='home'),
+    path('add-category/',views.addCategory,name='add_category'),
+    path('all-movies/',views.allMovies,name='all_movies'),
+    path('movie-detail/<int:movie_id>',views.movieDetails,name='movie_details'),
+    path('delete-movie/<int:movie_id>',views.deleteMovie,name='delete_movie'),
+    path('add-movie/',views.addMovies,name='add_movie'),
+    path('update-movie/<int:movie_id>',views.updateMovie,name='update_movie'),
+    path('add-cast/',views.addCast,name='add_cast')
 ]
